5f3fb4a0676a9fad4712cd10d1d8bfcd5a7d4bc3
CreativeMind012 <88550031+CreativeMind012@users.noreply.github.com>
2022-04-28 18:41:50 +0530

    Add files via upload

---
+++ AC.js
@@ -0,0 +1,29 @@
+/*
+
+*/
+status = "";
+img = "";
+
+function preload(){
+    img = loadImage('AC.jpg');
+}
+function setup(){
+    canvas = createCanvas(380, 420);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+function modelLoaded(){
+    status = "true";
+    console.log("Model Loaded!");
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results)
+    }
+}
